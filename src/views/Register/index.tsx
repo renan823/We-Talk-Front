38e64de48076fb06f81b424190ecf6f3d8717870
renan823 <renan.trofino@gmail.com>
2023-06-01 10:33:41 -0300

    fix login style

--- src/views/Register/index.tsx
+++ src/views/Register/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import {
   SafeAreaView,
+  ScrollView,
   Text,
+  TextInput,
+  TouchableHighlight,
   View
 } from 'react-native';
 
@@ -14,9 +17,36 @@ import { StackProps } from '../../configs/navigation.config';
 const Register: React.FC<StackProps<'Register'>> = ({ navigation }) => {
   return(
     <SafeAreaView>
-      <View>
-        <Text>Tela de cadastro =)</Text>
+      <ScrollView>
+        <View>
+          <View></View>
+        </View>
+        <View>
+        <View style={styles.label}>
+          <Text style={styles.labelText}>Nome</Text>
+          <TextInput style={styles.input}/>
+        </View>
+        <View style={styles.label}>
+          <Text style={styles.labelText}>Senha</Text>
+          <TextInput style={styles.input}/>
+        </View>
+        <View style={styles.label}>
+          <Text style={styles.labelText}>Senha</Text>
+          <TextInput style={styles.input}/>
+        </View>
+        <View style={styles.label}>
+          <Text style={styles.labelText}>Senha</Text>
+          <TextInput style={styles.input}/>
+        </View>
+        <View style={styles.label}>
+          <Text style={styles.labelText}>Senha</Text>
+          <TextInput style={styles.input}/>
+          <TouchableHighlight style={styles.button}>
+            <Text style={styles.buttonText}>Login</Text>
+          </TouchableHighlight>
+        </View>
       </View>
+      </ScrollView>
     </SafeAreaView>
   )
 }
